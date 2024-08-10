c07a60db6acaefd981e9e631bad5a2a81483a5fd
Darwin Rivera <riveradarwin.1991@outlook.com>
2024-08-09 21:32:25 -0500

    se hacen las primeeras lineas de codigo

--- EjerciciosJS/Ejercicio-2/JS/logica.js
+++ EjerciciosJS/Ejercicio-2/JS/logica.js
@@ -1,2 +1,28 @@
-const usuarios = [{ cedula: '88123456', nombre: 'Edwin', telefono: '320' , edad: '40' },
-{ cedula: '1090987654', nombre: 'Maria', telefono: '310' , edad: '16' }]
+console.log('conected')
+const usuarios = [ { cedula: '1', nombre: 'Edwin', telefono: '320' , edad: '45' },
+{ cedula: '2', nombre: 'Maria', telefono: '310' , edad: '16' },
+{ cedula: '3', nombre: 'Karina', telefono: '301' , edad: '34' },]
+
+const cedula= document.getElementById('cedula').value
+
+//cedula.addEventListener('click', () => {
+ function validarUsuario(){
+ let usuarioExiste= usuarios.find(usuario=> usuario.cedula==cedula) || alert('Usuario no existe')
+ console.log(usuarioExiste)
+ if(usuarioExiste){
+    if(usuarioExiste.edad>=18){
+
+     console.log(usuarioExiste.edad)
+         alert('Registro exitoso')
+     }else{
+        console.log(usuarioExiste.edad)
+
+         alert('Registro rechazado')
+       }}
+}
+
+
+
+// function validarEdad(edad){
+//     let edadUsuario= 
+// }
