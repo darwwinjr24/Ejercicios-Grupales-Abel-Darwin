09261e3496e25ed0f30d8cf5c18226f7434fb9d2
Darwin Rivera <riveradarwin.1991@outlook.com>
2024-08-11 16:47:18 -0500

    se cambio de posicion variable cedula

--- EjerciciosJS/Ejercicio-2/JS/logica.js
+++ EjerciciosJS/Ejercicio-2/JS/logica.js
@@ -3,11 +3,9 @@ const usuarios = [ { cedula: '1', nombre: 'Edwin', telefono: '320' , edad: '45' 
 { cedula: '2', nombre: 'Maria', telefono: '310' , edad: '16' },
 { cedula: '3', nombre: 'Karina', telefono: '301' , edad: '34' },]
 
-const cedula= document.getElementById('cedula').value
-
-//cedula.addEventListener('click', () => {
- function validarUsuario(){
- let usuarioExiste= usuarios.find(usuario=> usuario.cedula==cedula) || alert('Usuario no existe')
+  function validarUsuario(){
+   let cedula= document.getElementById('cedula').value
+let usuarioExiste= usuarios.find(usuario=> usuario.cedula==cedula) || alert('Usuario no existe')
  console.log(usuarioExiste)
  if(usuarioExiste){
     if(usuarioExiste.edad>=18){
@@ -19,7 +17,7 @@ const cedula= document.getElementById('cedula').value
 
          alert('Registro rechazado')
        }}
-}
+};
 
 
 
