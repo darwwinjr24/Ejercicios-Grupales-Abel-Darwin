da753b0e5ead9ef69ad2ccad8f84e449f0010071
ABEL CARVAJAL <joseabel.carvajal@gmail.com>
2024-08-13 11:18:47 -0500

    Aggregate lines to show information about the credit requested

--- EjerciciosJS/Ejercicio-4/JS/logica.js
+++ EjerciciosJS/Ejercicio-4/JS/logica.js
@@ -1,47 +1,86 @@
+let credType = document.getElementById("tipoCredito");
+let credito = document.getElementById("valorCredito");
+let cuotas = document.getElementById("cuotas");
+const btnCalcular = document.getElementById("calcular");
+const tabla = document.getElementById("tabla");
 
-const credType = document.getElementById('tipoCredito')
+let tasa;
+let interes;
+let descuento;
+let tipoCredito;
 
-const credito = document.getElementById('valorCredito').value
+btnCalcular.addEventListener("click", () => {
+    intereses();
 
-const cuotas = document.getElementById('cuotas')
+    console.log(`Tasa: ${tasa}. Interes: ${interes}. Cuotas: ${cuotas.value}`);
 
-const btnCalcular = document.getElementById('calcular')
+    //La ecuación para calcular la cuota mensual la obtuve de https://comparabien.com.pe/faq/como-calcular-cuota-prestamo.
+    //Sólo agregué el descuento tal como lo pide el ejercicio
 
-let tasa
-let interes
-let descuento
+    const mensualidad =
+        (credito.value * tasa * Math.pow(1 + tasa, cuotas.value)) /
+        (Math.pow(1 + tasa, cuotas.value) - 1) - descuento;
 
-btnCalcular.addEventListener('click', () => {
+    //////////////Inserción de tabla al modal////////////
 
-    intereses()
-    
-    console.log(`Tasa: ${tasa}. Interes: ${interes}. Cuotas: ${cuotas.value}`)
-
+    /* Limpia la tabla en caso de que ya tenga información */
+    const tbodyExist = document.getElementById('table-body')
+    if(tbodyExist){
+        tbodyExist.remove()
+    }
         
-    const mensualidad = ((credito * tasa * Math.pow((1+tasa),cuotas.value)) / (Math.pow((1+tasa),cuotas.value)-1)) - descuento
-    console.log( `El valor de la mensualidad es ${parseInt(mensualidad)}. El interes mensual es: ${interes}. Ud tiene un descuento mensual de: ${descuento}`  )
+    const cuerpoTabla = document.createElement("tbody");
+    cuerpoTabla.id = "table-body"
+    cuerpoTabla.innerHTML = `
+                                <tr>
+                                    <th scope="row">Monto solicitado</th>
+                                    <td>$${credito.value}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Tipo de crédito</th>
+                                    <td>${tipoCredito}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Interés mensual</th>
+                                    <td colspan="2">${(tasa * 100).toFixed(1)}%  ->  $${interes}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Número de cuotas</th>
+                                    <td colspan="2">${cuotas.value}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Valor cuota mensual</th>
+                                    <td colspan="2">$${parseInt(mensualidad)}</td>
+                                </tr>
+                            `;
 
+    tabla.appendChild(cuerpoTabla);
+
+    credType.value = "";
+    credito.value = "";
+    cuotas.value = "";
 });
 
 
-function intereses(){
-    descuento = 0
-    if(credType.value == "LII"){
-        tasa = 0.025
+function intereses() {
+    descuento = 0;
+    if (credType.value == "LII") {
+        tasa = 0.025;
+        tipoCredito = "Libre inversión";
     } else {
-        tasa = 0.029
+        tasa = 0.029;
+        tipoCredito = "Libranza";
     }
-    interes = credito * tasa
+    interes = credito.value * tasa;
 
-    if(cuotas.value < 6){
-        descuento = interes * 0.2
+    if (cuotas.value < 6) {
+        descuento = interes * 0.2;
     }
-    if(cuotas.value > 12 && cuotas.value < 24){
-        descuento = interes * 0.4
+    if (cuotas.value > 12 && cuotas.value < 24) {
+        descuento = interes * 0.4;
     }
-    if(cuotas.value > 24){
-        descuento = interes * 0.7
+    if (cuotas.value > 24) {
+        descuento = interes * 0.7;
     }
-
 }
 
